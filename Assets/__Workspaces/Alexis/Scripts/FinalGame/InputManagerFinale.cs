8523f0d81042421a517a6d5c35b00fcd2383534a
Alexis Verne-rey <a.verne@nouvellesimages.xyz>
2025-12-10 11:42:56 +0100

    Revert "Advancement on ending game"
    This reverts commit 3eeeb107088f832f39562e799c09be3ab45c3551.

--- Assets/__Workspaces/Alexis/Scripts/FinalGame/InputManagerFinale.cs
+++ Assets/__Workspaces/Alexis/Scripts/FinalGame/InputManagerFinale.cs
@@ -6,12 +6,10 @@ public class InputManagerFinale : MonoBehaviour
 {
     public static event Action<bool> OnInputDeviceChanged;
     [SerializeField] private GameObject _UiScriptManager;
-    [SerializeField] private GameObject _Bomb;
 
     private PlayerInput _playerInput;
     private PlayerControllerFinal _playerControllerFinal;
     private InGamePause _inGamePause;
-    private DeactivateBomb _deactivateBomb;
     private bool _isControllerConnected;
     
 
@@ -20,7 +18,6 @@ public class InputManagerFinale : MonoBehaviour
         _playerInput = GetComponent<PlayerInput>();
         _playerControllerFinal = GetComponent<PlayerControllerFinal>();
         _inGamePause = _UiScriptManager.GetComponent<InGamePause>();
-        _deactivateBomb = _Bomb.GetComponent<DeactivateBomb>();
         if (_playerInput == null) throw new NullReferenceException("PlayerInputManager is null");
     }
     
@@ -118,15 +115,7 @@ public class InputManagerFinale : MonoBehaviour
 
     private void OnJetpack(InputAction.CallbackContext context)
     {
-        if (!_playerControllerFinal.CanDeactivateBomb)
-        {
-            _playerControllerFinal.UseJetpack = context.ReadValueAsButton();
-        }
-
-        if (_playerControllerFinal.CanDeactivateBomb)
-        {
-            _deactivateBomb.EndGame();
-        }
+        _playerControllerFinal.UseJetpack = context.ReadValueAsButton();
     }
 
     private void OnPauseMenuOpened(InputAction.CallbackContext context)
--- Assets/__Workspaces/Alexis/Scripts/FinalGame/PlayerControllerFinal.cs
+++ Assets/__Workspaces/Alexis/Scripts/FinalGame/PlayerControllerFinal.cs
@@ -45,9 +45,6 @@ public class PlayerControllerFinal : MonoBehaviour
     public BaseState PreviousState;
     private BaseState _currentState;
     
-    //Bomb
-    public bool CanDeactivateBomb;
-    
     //Animator
 	public Animator PlayerAnimator;
     
