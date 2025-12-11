5cff6f9dadbb02ea68a7cb45fd3c5c64b8abb865
Alexis Verne-rey <a.verne@nouvellesimages.xyz>
2025-12-11 12:15:42 +0100

    CrawlerUpdate

--- Assets/__Workspaces/Tom/New Folder/HideExplosion.cs
+++ Assets/__Workspaces/Tom/New Folder/HideExplosion.cs
@@ -2,21 +2,14 @@ using UnityEngine;
 
 public class ExplosionFX : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Hide()
     {
         gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     public void DestroySelf()
     {
         Destroy(gameObject);
-    }
-
-    public void DisableChild(GameObject child)
-    {
-        child.SetActive(false);
     }
 }
 
