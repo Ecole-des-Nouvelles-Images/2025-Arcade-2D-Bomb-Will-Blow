9db7ed4290cc55b6f85d45f4032486e89ce2c95c
Alexis <a.verne@nouvellesimages.xyz>
2025-12-13 15:48:40 +0100

    minor changes on UI and enemy sizes

--- Assets/__Workspaces/Alexis/Scripts/FinalGame/Enemies/Crawler/CrawlerBehavior.cs
+++ Assets/__Workspaces/Alexis/Scripts/FinalGame/Enemies/Crawler/CrawlerBehavior.cs
@@ -60,12 +60,12 @@ public class CrawlerBehaviour : MonoBehaviour , IKillable
     
         void GoToUpperLimit()
         {
-            _rb.AddForce(new Vector2(0,100 * Time.deltaTime));
+            _rb.AddForce(new Vector2(0,10000 * Time.deltaTime));
         }
 
         void GoToLowerLimit()
         {
-            _rb.AddForce(new Vector2(0,-100  * Time.deltaTime));
+            _rb.AddForce(new Vector2(0,-10000  * Time.deltaTime));
         }
 
         public void Kill() 
