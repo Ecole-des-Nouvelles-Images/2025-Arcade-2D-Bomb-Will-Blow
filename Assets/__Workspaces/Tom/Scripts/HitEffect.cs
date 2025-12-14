55c7d0b550009ff2efa09379db29e17d521fb98b
Tom <t.goret@nouvellesimages.xyz>
2025-12-14 22:43:19 +0100

    Dash Jump et Explosion améliorés,  glow + Hit effect Script ( à test)

---
+++ Assets/__Workspaces/Tom/Scripts/HitEffect.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections;
+
+public class HitEffect : MonoBehaviour
+
+   
+    
+    {
+        #region Datamembers
+
+        #region Editor Settings
+
+        [Tooltip("Material to switch to during the flash.")]
+        [SerializeField] private Material flashMaterial;
+
+        [Tooltip("Duration of the flash.")]
+        [SerializeField] private float duration;
+
+        #endregion
+        #region Private Fields
+
+        
+        private SpriteRenderer spriteRenderer;
+
+        
+        private Material originalMaterial;
+
+        
+        private Coroutine flashRoutine;
+
+        #endregion
+
+        #endregion
+
+
+        #region Methods
+
+        #region Unity Callbacks
+
+        void Start()
+        {
+           
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+            
+            originalMaterial = spriteRenderer.material;
+        }
+
+        #endregion
+
+        public void Flash()
+        {
+            // If the flashRoutine is not null, then it is currently running.
+            if (flashRoutine != null)
+            {
+                // In this case, we should stop it first.
+                // Multiple FlashRoutines the same time would cause bugs.
+                StopCoroutine(flashRoutine);
+            }
+
+            // Start the Coroutine, and store the reference for it.
+            flashRoutine = StartCoroutine(FlashRoutine());
+        }
+
+        private IEnumerator FlashRoutine()
+        {
+            // Swap to the flashMaterial.
+            spriteRenderer.material = flashMaterial;
+
+            // Pause the execution of this function for "duration" seconds.
+            yield return new WaitForSeconds(duration);
+
+            // After the pause, swap back to the original material.
+            spriteRenderer.material = originalMaterial;
+
+            // Set the routine to null, signaling that it's finished.
+            flashRoutine = null;
+        }
+
+        #endregion
+    }
