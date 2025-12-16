9d90c779ed7f0bfe4fe28d08687040ec6c98c059
Wren <m.feliza@nouvellesimages.xyz>
2025-12-16 09:40:47 +0100

    Revert "Merge branch 'int' into Wren"
    This reverts commit 46a76cbcb4f74e77a27f07a9898d16f6d9919090, reversing
    changes made to c278fd83fdfca76e99f8d54861f816dc283f1fa3.

--- Assets/__Workspaces/Alexis/Scripts/FinalGame/DeactivateBomb.cs
+++ Assets/__Workspaces/Alexis/Scripts/FinalGame/DeactivateBomb.cs
@@ -12,27 +12,10 @@ public class DeactivateBomb : MonoBehaviour
     private bool _playerControllerP1Found;
     private bool _playerControllerP2Found;
 
-    private void Awake() {
-        InputManagerFinale.OnPlayer1Spawn += Player1Spawn;
-        InputManagerFinale.OnPlayer2Spawn += InputManagerFinaleOnOnPlayer2Spawn;
-    }
-
-    private void OnDestroy() {
-        InputManagerFinale.OnPlayer1Spawn -= Player1Spawn;
-        InputManagerFinale.OnPlayer2Spawn -= InputManagerFinaleOnOnPlayer2Spawn;
-    }
-
-    private void InputManagerFinaleOnOnPlayer2Spawn(PlayerControllerFinal playerControllerFinal) {
-        _playerControllerP2 = playerControllerFinal;
-    }
-
-    private void Player1Spawn(PlayerControllerFinal playerControllerFinal) {
-        _playerControllerP1 = playerControllerFinal;
-    }
     // Update is called once per frame
     void Update()
     { 
-        /*if (!_playerControllerP1Found)
+        if (!_playerControllerP1Found)
         {
             _p1 = GameObject.FindGameObjectWithTag("Player");
             _playerControllerP1 =  _p1.GetComponent<PlayerControllerFinal>();
@@ -45,13 +28,12 @@ public class DeactivateBomb : MonoBehaviour
         if (!_playerControllerP2Found)
         {
             _p2 = GameObject.FindGameObjectWithTag("Player2");
-            if (_p2 == null) return;
             _playerControllerP2 = _p2.GetComponent<PlayerControllerFinal>();
             if (_playerControllerP2 == _playerControllerP2.GetComponent<PlayerControllerFinal>())
             {
                 _playerControllerP2Found = true; 
             }
-        }*/
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
