3322084d3b9b12d1da12a5015db9191718e2184c
Alexis Verne-rey <a.verne@nouvellesimages.xyz>
2025-12-16 11:45:59 +0100

    work

--- Assets/__Workspaces/Alexis/Scripts/FinalGame/UI/ReplayGame.cs
+++ Assets/__Workspaces/Alexis/Scripts/FinalGame/UI/ReplayGame.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 public class ReplayGame : MonoBehaviour
 {
     private Scene currentScene;
-
+    
+    [SerializeField] private GameObject _VictoryScreenFromDeath;
+    [SerializeField] private GameObject _VictoryScreenFromBomb;
+    
     private void Update()
     {
         currentScene = SceneManager.GetActiveScene();
@@ -13,6 +16,9 @@ public class ReplayGame : MonoBehaviour
 
     public void Replay()
     {
+        _VictoryScreenFromBomb.SetActive(false);
+        _VictoryScreenFromDeath.SetActive(false);
         SceneManager.LoadScene(currentScene.name);
+        Time.timeScale = 1;
     }
 }
