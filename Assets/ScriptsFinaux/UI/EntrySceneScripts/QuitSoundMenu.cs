9a2bf64958407d1911c9fae6dca982d3ae837352
Alexis Verne-rey <a.verne@nouvellesimages.xyz>
2025-12-19 13:24:27 +0100

    final tweaks

--- Assets/ScriptsFinaux/UI/EntrySceneScripts/QuitSoundMenu.cs
+++ Assets/ScriptsFinaux/UI/EntrySceneScripts/QuitSoundMenu.cs
@@ -4,10 +4,12 @@ public class QuitSoundMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _SoundMenu;
     [SerializeField] private GameObject _MainMenu;
+    [SerializeField] private GameObject _Ninjas;
 
     public void QuitSoundParameters()
     {
         _SoundMenu.SetActive(false);
+        _Ninjas.SetActive(true);
         _MainMenu.SetActive(true);
     }
 }
--- Assets/ScriptsFinaux/UI/EntrySceneScripts/SoundMenu.cs
+++ Assets/ScriptsFinaux/UI/EntrySceneScripts/SoundMenu.cs
@@ -4,10 +4,12 @@ public class SoundMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _SoundMenu;
     [SerializeField] private GameObject _MainMenu;
-
+    [SerializeField] private GameObject _Ninjas;
+    
     public void SoundParameters()
     {
         _MainMenu.SetActive(false);
+        _Ninjas.SetActive(false);
         _SoundMenu.SetActive(true);
     }
 }
